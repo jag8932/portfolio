df54a83ccc80677a772f7d2dc06b0da4527f2d62
jag8932 <jacob.a.goodwillie@gmail.com>
2022-04-12 14:00:43 -0400

    Style changes

--- router.js
+++ router.js
@@ -5,9 +5,17 @@ function route(app) {
         res.sendFile(path.join(__dirname, '/index.html'));
     });
 
+    app.get('/projects', (req, res) => {
+        res.sendFile(path.join(__dirname, '/projects.html'));
+    });
+
+    app.get('/contactInfo', (req, res) => {
+        res.sendFile(path.join(__dirname, '/contact.html'));
+    });
+
     app.get('/styles/styles1.css', (req, res) => {
         res.sendFile(path.join(__dirname,'/styles/styles1.css'));
-    })
+    });
 }
 
 module.exports = {
